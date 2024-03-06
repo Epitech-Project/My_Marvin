fe577d0cf6656e25113c025303ee029d8bd897e4
Thomaltarix <thomas.boue@epitech.eu>
2024-03-06 13:34:11 +0100

    :wrench: Add the Clone repository job

---
+++ job_dsl.groovy
@@ -0,0 +1,16 @@
+folder('Tools')
+freeStyleJob('Tools/clone-repository') {
+    // Clean the workspace before running the job
+    // wrappers {
+    //     prebuildCleanup()
+    // }
+    // Define parameters
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    // Clone the repository
+    steps {
+        shell('git clone ${GIT_REPOSITORY_URL}')
+    }
+}
+
